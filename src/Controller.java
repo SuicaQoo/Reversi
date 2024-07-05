0596402708d3ab8a2a630cc247a602c84bea2051
22FI024HaruyaOno <22fi024@ms.dendai.ac.jp>
2024-07-05 10:01:01 +0900

    changeReversi
    Co-authored-by: 22fi028 <22fi028@users.noreply.github.com>

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -6,11 +7,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
+
+
 public class Controller extends JFrame implements MouseListener{
-    JPanel pane;
-    JPanel board;
-    JProgressBar timeLimitBar;
-    Model model;
+    JPanel pane; // contentPane
+    JPanel board; // ボード
+    JProgressBar timeLimitBar; // 時間制限バー
+    Model model; // データ保持変数
+	
 
     // main
     public static void main(String arg[]){
@@ -18,18 +22,24 @@ public class Controller extends JFrame implements MouseListener{
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 520);
-
     }
 
     // コンストラクタ
     public Controller(){
         pane = (JPanel)getContentPane();
-        //pane.setLayout(new BorderLayout());
+        pane.setLayout(new BorderLayout());
         pane.setBackground(Color.WHITE);
-        //model = new 
-        timeLimitBar = new TimeLimit();
+        //model = new model();
+        timeLimitBar = new TimeLimitBar();
         add(timeLimitBar);
-        
+
+		// 制限時間バーの追加
+		timeLimitBar = new TimeLimitBar();
+		pane.add(timeLimitBar, BorderLayout.NORTH);
+
+		// ボードの追加
+        JPanel board = new View();
+		pane.add(board, BorderLayout.CENTER);
     }
 
 	@Override
