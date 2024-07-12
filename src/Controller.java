b33f1221cb88aed02258afba9fef31838042ffbf
22FI024HaruyaOno <22fi024@ms.dendai.ac.jp>
2024-07-12 11:23:01 +0900

    Changed
    Co-authored-by: 22fi028 <22fi028@users.noreply.github.com>

--- src/Controller.java
+++ src/Controller.java
@@ -1,46 +1,77 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
 
+public class Controller extends JFrame implements MouseListener {
+	private JPanel pane; // contentPane
+	TimeLimitBar timeLimitBar; // 時間制限バー
+	Model model; // データ保持変数
+	View board; // 描画変数
+	Dimension dim;
 
+	// main
 
-public class Controller extends JFrame implements MouseListener{
-    JPanel pane; // contentPane
-    JPanel board; // ボード
-    JProgressBar timeLimitBar; // 時間制限バー
-    Model model; // データ保持変数
-	
+	/*
+	 * public static void main(String arg[]) {
+	 * Controller frame = new Controller();
+	 * frame.setVisible(true);
+	 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 * frame.setSize(500, 520);
+	 * frame.setDimension();
+	 * }
+	 *
+	 */
 
-    // main
-    public static void main(String arg[]){
-        JFrame frame = new Controller();
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 520);
-    }
+	// main
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			Controller controller = new Controller();
+			controller.setSize(500, 535);
+			controller.setVisible(true);
+			// controller.setDimension();
+		});
+	}
 
-    // コンストラクタ
-    public Controller(){
-        pane = (JPanel)getContentPane();
-        pane.setLayout(new BorderLayout());
-        pane.setBackground(Color.WHITE);
-        //model = new model();
-        timeLimitBar = new TimeLimitBar();
-        add(timeLimitBar);
+	// コンストラクタ
+	public Controller() {
+		pane = (JPanel) getContentPane();
+		pane.setLayout(new BorderLayout());
+		pane.setBackground(Color.WHITE);
+		// model = new model();
+		// System.out.println("width =" + get + " height = " + pane.getHeight());
 
 		// 制限時間バーの追加
 		timeLimitBar = new TimeLimitBar();
 		pane.add(timeLimitBar, BorderLayout.NORTH);
 
+		// テスト
+		timeLimitBar.start();
+
 		// ボードの追加
-        JPanel board = new View();
-		pane.add(board, BorderLayout.CENTER);
-    }
+		initializeView();
+		// board = new View();
+		// pane.add(board, BorderLayout.CENTER);
+	}
+
+	private void initializeView() {
+		board = new View(this); // Controllerのインスタンス（this）をViewに渡します
+		setView(board); // boardをフレームに追加
+	}
+
+	public void setView(View view) {
+		pane.add(view, BorderLayout.CENTER);
+	}
+
+	public void setViewSize(Dimension dim) {
+		// Viewのサイズを更新するための処理
+		board.setSize(dim);
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
@@ -70,5 +101,19 @@ public class Controller extends JFrame implements MouseListener{
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+	}
+
+	public JPanel getPane() {
+		return pane;
+	}
+
+	public void setDimension() {
+		dim = getContentPane().getSize();
+		System.out.println("width = " + dim.getWidth() + " height = " + dim.getHeight());
+		setViewSize(dim);
+	}
+
+	public Dimension getDim() {
+		return dim;
 	}
 }
